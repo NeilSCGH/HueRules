8cf48ee5d068b8195ef48aa67a077b2db8c42033
NeilSCGH <neil.segard@gmail.com>
2021-11-13 19:05:37 +0100

    adding details

--- js/script.js
+++ js/script.js
@@ -7,35 +7,63 @@ function printRulesToTable(data){
   for (const [indexRule, rule] of Object.entries(data)) {
     var row = table.insertRow();
 
-    var name = rule["name"];
-    row.insertCell(0).innerHTML = name;
+    row.insertCell(0).innerHTML = indexRule;
 
     var conditionsText = "";
     for (const [indexCond, condition] of Object.entries(rule["conditions"])) {
-      conditionsText = conditionsText + "<br>" + condition["address"];
+      if (conditionsText != "") {
+        conditionsText = conditionsText + "<br>";
+      }
+
+      conditionsText = conditionsText + condition["address"]
+                                + " " + condition["operator"];
+
+      if (condition["value"] != undefined){
+        conditionsText = conditionsText + " " + condition["value"];
+      }
     }
     row.insertCell(1).innerHTML = conditionsText;
 
     var ActionsText = "";
     for (const [indexAct, action] of Object.entries(rule["actions"])) {
-      ActionsText = ActionsText + "<br>" + action["address"];
+      if (ActionsText != "") {
+        ActionsText = ActionsText + "<br>";
+      }
+
+      ActionsText = ActionsText + action["address"]
+                          + " " + action["method"];
+
+      for (const [desc, bodyAction] of Object.entries(action["body"])) {
+        ActionsText = ActionsText + " " + desc + " to " + bodyAction;
+      }
     }
     row.insertCell(2).innerHTML = ActionsText;
   }
 }
 
+function addMessage(txt){
+  $("#rulesTable tbody tr").remove();
+  var table = document.getElementById("rulesTable").getElementsByTagName('tbody')[0];
+  var row = table.insertRow();
+  var cell = row.insertCell(0);
+  cell.innerHTML = txt;
+  cell.colSpan = "3";
+  return cell;
+}
 
 function getRules(){
+  $("#rulesTable tbody tr").remove();
   var ip = $("#HubIP")[0].value;
   var apiKey = $("#APIKey")[0].value;
 
   console.log('Getting the rules...');
+  addMessage('Getting the rules...');
   console.log("http://" + ip + "/api/" + apiKey + "/rules");
   $.ajax({
       type: "GET",
       url: "http://" + ip + "/api/" + apiKey + "/rules",
       contentType: 'application/json'
-      })
+    })
     .fail(function(jqXHR, textStatus) {
       console.log('Error when reaching the hub');
     })
@@ -43,6 +71,5 @@ function getRules(){
       console.log('Done');
       console.log(response);
       printRulesToTable(response);
-    }
-  ) 
+    }) 
 }
